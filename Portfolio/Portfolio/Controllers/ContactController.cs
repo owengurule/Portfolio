e094d19314975c6fc4d87f816aa6f27b44c78852
SeedPaths-User <lessdismissive@email.com>
2014-09-29 13:00:59 -0600

    Updates
    Updates

---
+++ Portfolio/Portfolio/Controllers/ContactController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /Contact/
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            return View(new Models.Contact_Information());
+        }
+
+        [HttpPost]
+        public ActionResult Contact(Models.Contact_Information contactForm)
+        {
+            Models.Contact_Information db = new Models.Contact_Information();
+
+            db.Contact_Information.Add(contactForm);
+
+            db.SaveChanges();
+
+            return RedirectToAction("ThankYou", "Contact");
+        }
+
+        public ActionResult ThankYou()
+        {
+            return View();
+        }
+    }
+}
---
+++ Portfolio/Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Container1()
+        {
+            return View();
+        }
+
+        public ActionResult Container2()
+        {
+            return View();
+        }
+
+    }
+}
