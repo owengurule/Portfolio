af99446089ae045a7ad7afc4b0b80cc80bea56a3
SeedPaths-User <lessdismissive@email.com>
2014-10-06 12:05:24 -0600

    Updates

--- Portfolio/Portfolio/Controllers/WorkController.cs
+++ Portfolio/Portfolio/Controllers/WorkController.cs
@@ -31,5 +31,10 @@ namespace Portfolio.Controllers
             return View();
         }
 
+        public ActionResult Guessthatnumber()
+        {
+            return View();
+        }
+
     }
 }
